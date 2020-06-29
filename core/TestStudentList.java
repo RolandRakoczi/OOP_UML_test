4fa6c8501aa29ad81a1dcd1e4a16400cd940bb84
Roli <rakocziroland@yahoo.com>
2020-06-29 20:46:36 +0300

    another commit

--- core/TestStudentList.java
+++ core/TestStudentList.java
@@ -13,7 +13,7 @@ public class TestStudentList{
 		Student s3 = new Student("Anne",29,"Law");
 		Student s4 = new Student("Sara",37,"Arts");
 		
-		list = new StudentList(4);
+		list = new StudentList(0);
 		assert(s1.getAge()<110):"The entered age is not a valid age: "+s1.getAge();
 		assert(s2.getAge()<110):"The entered age is not a valid age: "+s2.getAge();
 		assert(s3.getAge()<110):"The entered age is not a valid age: "+s3.getAge();
@@ -22,12 +22,15 @@ public class TestStudentList{
 		System.out.println("Test empty list:");
 		TestStudentList.addZeroStudent();
 		System.out.println("");
+		list = new StudentList(1);
 		System.out.println("Test the list with one element:");		
 		TestStudentList.addOneStudent(s1);
 		System.out.println("");
+		list = new StudentList(2);
 		System.out.println("Test the list with two elements:");	
 		TestStudentList.addTwoStudents(s1,s2);
 		System.out.println("");
+		list = new StudentList(4);
 		System.out.println("Test the list with four elements:");
 		TestStudentList.addAll(s1,s2,s3,s4);
 	}
@@ -37,22 +40,34 @@ public class TestStudentList{
 	}
 	
 	public static void addOneStudent(Student s1){
+		try{
 		list.addStudent(s1);
 		TestStudentList.listing();
+		}catch(ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public static void addTwoStudents(Student s1,Student s2){
+		try{
 		list.addStudent(s1);
 		list.addStudent(s2);
 		TestStudentList.listing();
+		}catch(ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public static void addAll(Student s1,Student s2,Student s3,Student s4){
+		try{
 		list.addStudent(s1);
 		list.addStudent(s2);
 		list.addStudent(s3);
 		list.addStudent(s4);
 		TestStudentList.listing();
+		}catch(ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public static void listing(){
